c86c5013ed2f445c58eb6eff0e672523486589d1
Hello <deftqml@gmail.com>
2022-08-02 22:05:18 +0700

    a

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  apiKey: "AIzaSyBoHMiQu7hGAyTpt9kpleO_5YuHIQHnY5Q",
+  authDomain: "nextjs-chatapp-70d39.firebaseapp.com",
+  projectId: "nextjs-chatapp-70d39",
+  storageBucket: "nextjs-chatapp-70d39.appspot.com",
+  messagingSenderId: "947374015005",
+  appId: "1:947374015005:web:0554641358f79be9c33cf4",
+  measurementId: "G-QXR8VCEDM1"
 };
 
 firebase.initializeApp(firebaseConfig);
